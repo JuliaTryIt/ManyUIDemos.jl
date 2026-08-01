174fe4b364f63ad45096a191e66526c138ac242b
s-celles <s.celles@gmail.com>
2026-08-01 23:33:55 +0200

    test: add tests to compile demos

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,15 @@ using TestItemRunner
     Aqua.test_all(ManyUIDemos)
 end
 
+@testitem "Demos Compile" begin
+    demos_dir = joinpath(pkgdir(ManyUIDemos), "demos")
+    for file in readdir(demos_dir)
+        if endswith(file, ".jl")
+            # Including the file will compile its definitions and throw if there's a syntax error
+            # We don't run the `main()` function because `abspath(PROGRAM_FILE)` is not `@__FILE__` during tests.
+            @test_nowarn include(joinpath(demos_dir, file))
+        end
+    end
+end
+
 @run_package_tests
